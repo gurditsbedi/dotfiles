22843ed9cb8974594c51cdd03d414fa1f5baf978
Gurdit Singh Bedi <gurditsbedi@gmail.com>
2020-05-31 13:46:34 +0530

    Add JioBattery Script for i3status-rs

--- jiofi_battery.sh
+++ jiofi_battery.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env sh
 
 rawhtml=`curl -s  'http://jiofi.local.html/'`
-[ $? -gt 0 ] && echo "" && exit 1 # exit if curl failed
+[ $? -gt 0 ] && echo "{\"text\": \"\"}" && exit 1 # exit if curl failed
 
 html=`echo $rawhtml | tr -d '\000'`
 battery_level=`echo $html | grep -oE "[0-9]+%" | rev | cut -c 2- | rev`
 
-_=$(echo $html | grep -i -o "discharging")
-[ $? = 0 ] && echo "JioFi $battery_level% ▼" || echo "JioFi $battery_level% ▲"
+symbol="▲"
+[ $(echo $html | grep -i -o "discharging") ] && symbol="▼"
+
+state="Idle"
+[ "$battery_level" -le "20"  ] && state="Warning"
+[ "$battery_level" -le "10"  ] && state="Critical"
+
+echo "{\"text\": \" JioFi $battery_level%$symbol\", \"state\":\"$state\"}"
